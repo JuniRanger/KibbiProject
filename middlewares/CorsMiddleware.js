30dfebcf73bcb87c0e52adc1a27a48a27d2603ec
JuniRanger <raincolo54@gmail.com>
2024-11-29 09:24:13 -0600

    cors guardado

--- middlewares/CorsMiddleware.js
+++ middlewares/CorsMiddleware.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 const allowedDomains = [
     'http://127.0.0.1:5500',
     'https://frontapi-0cuw.onrender.com/',
-    'http://localhost:3000',
+    'http://localhost:5000',
     'https://kibbiapi.onrender.com'
     
 ];
